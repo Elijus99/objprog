d0f55385b15f244f81a55c08f8617ed8b0fe70b6
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-02-04 15:27:49 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
-int main()
-{
+struct duomenys{
     char vardas[100];
     int dydis;
-    cout << "Koks Jusu vardas?" << endl;
-    cin >> vardas;
-    cout << "Iveskite remelio dydi" << endl;
-    cin >> dydis;
-    printf("***************************\n");
-    for(int i=0; i<dydis; i++)
-    {
-        //cout << "*                         *" << endl;
-        printf("%*c%*c\n", -10, '*', 17, '*');
-    }
-    printf("%*c%s%s%*c\n", -5, '*', "Sveikas, ", vardas, 4, '*');
-    for(int i=0; i<dydis; i++)
-    {
-        printf("%*c%*c\n", -10, '*', 17, '*');
-    }
-    printf("***************************\n");
+};
+duomenys ivedimas(duomenys A);
+void lentele(duomenys A);
+int main()
+{
+    duomenys A;
+    A = ivedimas(A);
+    lentele(A);
     return 0;
+}
+duomenys ivedimas(duomenys A)
+{
+    cout << "Koks Jusu vardas?" << endl;
+    cin >> A.vardas;
+    cout << "Iveskite remelio dydi" << endl;
+    cin >> A.dydis;
+    return A;
+}
+void lentele(duomenys A)
+{
+    printf("***************************\n");
+    for(int i=0; i<A.dydis; i++)
+    {
+        printf("%*c%*c\n", -10, '*', 17, '*');
+    }
+    printf("%*c%s%s%*c\n", -5, '*', "Sveikas, ", A.vardas, 4, '*');
+    for(int i=0; i<A.dydis; i++)
+    {
+        printf("%*c%*c\n", -10, '*', 17, '*');
+    }
+    printf("***************************\n");
 }
